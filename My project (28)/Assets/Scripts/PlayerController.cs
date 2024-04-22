ce2592a265f0df302ac2a79abcc00263ce895a1a
Porton12345 <vkruk123@mail.ru>
2024-04-22 13:49:26 +0300

    5

--- My project (28)/Assets/Scripts/PlayerController.cs
+++ My project (28)/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
+using TMPro;
+using UnityEngine.UI;
 
-public class Mover : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     private const string Horizontal = nameof(Horizontal);
     private const string Vertical = nameof(Vertical);    
@@ -11,19 +13,47 @@ public class Mover : MonoBehaviour
     [SerializeField] private AnimationClip _clipIdle;
     [SerializeField] private AnimationClip _clipRunRight;
     [SerializeField] private AnimationClip _clipRunLeft;
+    [SerializeField] private TextMeshProUGUI _hpText;
+    [SerializeField] private Slider _hpSlider;
+    [SerializeField] private Slider _hpSmoothSlider;
 
     private Vector2 _distance = Vector3.zero;
     private float _koefOfSpeed = 0.01f;
-    private int _currentHealth = 100;
-    private int _maxHealth = 100;
-    private Coroutine _moverCoroutine;
+    private float _currentHealth = 100f;
+    private float _maxHealth = 100f;
+    private Coroutine _damageCoroutine;
+    private float _maxDelta = 100f;
+    private int _delay = 1;
+    private int _buttonDamage = 10;
+    private float _currentSmoothHealth;
 
     public int Damage => 10;
     public float Delay => 0.1f;
 
+    public void OnClickDamageButton()
+    {               
+        WaitForSeconds wait = new WaitForSeconds(_delay);
+        _damageCoroutine = StartCoroutine(TakeButtonDamage(_buttonDamage, wait));        
+    }
+
+    public void OnClickHealButton()
+    {
+        _currentHealth = _maxHealth;       
+    }  
+
+    private void Start()
+    {
+        _hpText.text = "100/100";
+        _currentSmoothHealth = _maxHealth;
+    }
+
     private void Update()
     {
-        Move();       
+        Move();
+        _hpText.text = _currentHealth.ToString("") + "/100";
+        _hpSlider.value = _currentHealth;           
+        _currentSmoothHealth = Mathf.MoveTowards(_currentSmoothHealth, _currentHealth, _maxDelta * Time.deltaTime);
+        _hpSmoothSlider.value = _currentSmoothHealth;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -41,28 +71,28 @@ public class Mover : MonoBehaviour
 
         if (collision.gameObject.TryGetComponent(out EnemyMover enemy))
         {
-            if (_moverCoroutine != null)
+            if (_damageCoroutine != null)
             {
-                StopCoroutine(_moverCoroutine);
-                _moverCoroutine = null;
+                StopCoroutine(_damageCoroutine);
+                _damageCoroutine = null;
             }
             
-            if (_moverCoroutine == null)
+            if (_damageCoroutine == null)
             {                
                 WaitForSeconds wait = new WaitForSeconds(enemy.Delay);
-                _moverCoroutine = StartCoroutine(TakeDamage(enemy.Damage, wait));
+                _damageCoroutine = StartCoroutine(TakeDamage(enemy.Damage, wait));
             }
         }
-    }
+    }      
 
     private void OnTriggerExit2D(Collider2D collisiion)
     {
         if (collisiion.gameObject.TryGetComponent(out EnemyMover enemy))
         {
-            if (_moverCoroutine != null)
+            if (_damageCoroutine != null)
             {
-                StopCoroutine(_moverCoroutine);
-                _moverCoroutine = null;
+                StopCoroutine(_damageCoroutine);
+                _damageCoroutine = null;
             }
         }
     }
@@ -93,12 +123,19 @@ public class Mover : MonoBehaviour
     }
 
     private IEnumerator TakeDamage(int damage, WaitForSeconds wait)
-    {
+    {       
         while (true)
-        {
+        {            
             _currentHealth = Mathf.Clamp(_currentHealth - damage, 0, _maxHealth);
-            Debug.Log("HP игрока " + _currentHealth);
+            Debug.Log("HP игрока " + _currentHealth);            
             yield return wait;
         }
+    }
+
+    private IEnumerator TakeButtonDamage(int damage, WaitForSeconds wait)
+    {        
+            _currentHealth = Mathf.Clamp(_currentHealth - damage, 0, _maxHealth);
+            Debug.Log("HP игрока " + _currentHealth);
+            yield return wait;        
     }
 }
